7a1ef66b36e9b477e0965373c017215905c028ad
edwardwang618 <edward.wang618@gmail.com>
2021-10-19 00:04:57 -0700

    leet 30 with string hash, leet32

--- 30.substring-with-concatenation-of-all-words.cpp
+++ 30.substring-with-concatenation-of-all-words.cpp
@@ -7,26 +7,37 @@
 // @lc code=start
 class Solution {
 public:
+    using UL = unsigned long;
     vector<int> findSubstring(string s, vector<string>& words) {
         vector<int> res;
         int n = s.size();
+        unsigned long P = 131L;
+        auto hashS = vector<UL>(n + 1, 0), pow = vector<UL>(n + 1, 1);
+        for (int i = 0; i < n; i++) {
+            hashS[i + 1] = hashS[i] * P + s[i];
+            pow[i + 1] = pow[i] * P;
+        }
 
         int m = words.size();
         // words里每个单词出现次数，key是单词的哈希，value就是这个key出现次数
-        unordered_map<string, int> mp;
-        for (int i = 0; i < m; i++) mp[words[i]]++;
+        unordered_map<UL, int> mp;
+        for (int i = 0; i < m; i++) {
+            UL hash = 0;
+            for (char ch : words[i]) hash = hash * P + ch;
+            mp[hash]++;
+        }
 
         int k = words[0].size();
         for (int i = 0; i < n; i++) {
             if (i + k * m > n) break;
-            unordered_map<string, int> used;
+            unordered_map<UL, int> used;
             bool flag = true;
             // j是在截取第几个单词
             for (int j = 0; j < m; j++) {
                 int l = i + j * k, r = i + (j + 1) * k - 1;
-                string sub = s.substr(l, r - l + 1);
-                used[sub]++;
-                if (used[sub] > mp[sub]) {
+                UL hash = getHash(hashS, l, r, pow);
+                used[hash]++;
+                if (used[hash] > mp[hash]) {
                     flag = false;
                     break;
                 }
@@ -36,6 +47,10 @@ public:
         }
 
         return res;
+    }
+
+    unsigned long getHash(vector<UL> &hashS, int l, int r, vector<UL> &pow) {
+        return hashS[r + 1] - hashS[l] * pow[r - l + 1];
     }
 };
 // @lc code=end
