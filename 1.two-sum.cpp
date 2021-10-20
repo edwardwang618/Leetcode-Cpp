a4f414414c9022113c9eb435d750fbf54db053d1
edwardwang618 <edward.wang618@gmail.com>
2021-10-20 08:25:24 -0700

    leet 2

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -8,7 +8,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.count(target - nums[i])) return {mp[target - nums[i]], i};
+            mp[nums[i]] = i;
+        }
+
+        return {};
     }
 };
 // @lc code=end
