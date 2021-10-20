46b9f0f8d9c7f52f22d14430ac7ec03e46e33ac8
edwardwang618 <edward.wang618@gmail.com>
2021-10-19 17:51:28 -0700

    leet 3

--- 3.longest-substring-without-repeating-characters.cpp
+++ 3.longest-substring-without-repeating-characters.cpp
@@ -8,7 +8,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
+        int res = 0;
+        unordered_set<char> set;
+        for (int i = 0, j = 0; i < s.size(); i++) {
+            while (set.count(s[i])) {
+                set.erase(s[j]);
+                j++;
+            }
+
+            res = max(res, i - j + 1);
+            set.insert(s[i]);
+        }
+
+        return res;
     }
 };
 // @lc code=end
