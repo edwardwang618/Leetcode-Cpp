afd71370c7a574ae4bac81c441fff3188af0b1c7
edwardwang618 <edward.wang618@gmail.com>
2021-10-20 18:01:22 -0700

    leet 45

--- 45.jump-game-ii.cpp
+++ 45.jump-game-ii.cpp
@@ -8,7 +8,22 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        
+        int n = nums.size();
+        if (n <= 1) return 0;
+
+        int res = 0;
+        int l = 0, r = 0, far = 0;
+        while (l <= r) {
+            res++;
+            for (int i = l; i <= r; i++)
+                if (i + nums[i] >= n - 1) return res;
+                else far = max(far, i + nums[i]);
+
+            l = r + 1;
+            r = far;
+        }
+
+        return -1;
     }
 };
 // @lc code=end
