793c72633c3787f45c250e1e6fee8f9dad390d17
edwardwang618 <edward.wang618@gmail.com>
2022-05-17 20:27:36 -0700

    leet

--- 148.sort-list.cpp
+++ 148.sort-list.cpp
@@ -15,40 +15,55 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// [4, 2, 1] -> [2, 4, 1] -> [2, 4, 1]
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        if (!head || !head->next) return head;
-
-        ListNode *dummy = new ListNode(0), *slow = dummy, *fast = dummy;
+        ListNode *dummy = new ListNode(0);
         dummy->next = head;
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-
-        ListNode *l2 = slow->next;
-        slow->next = nullptr;
-        ListNode *l1 = sortList(dummy->next);
-        l2 = sortList(l2);
         ListNode *prev = dummy;
-        while (l1 || l2) {
-            if (!l1) {
-                prev->next = l2;
-                break;
+        for (int k = 1; ; k <<= 1) {
+            prev = dummy;
+            int cnt = 0;
+            while (prev->next) {
+                cnt++;
+                ListNode *slow = prev, *fast = prev;
+                for (int i = 0; i < k; i++) {
+                    if (slow) slow = slow->next;
+                    if (fast && fast->next) fast = fast->next->next;
+                }
+
+                if (!slow) break;
+                ListNode *l1 = prev->next, *l2 = slow->next, *tail = nullptr;
+                slow->next = nullptr;
+                if (fast && fast->next) {
+                    tail = fast->next;
+                    fast->next = nullptr;
+                }
+
+                while (l1 || l2) {
+                    if (!l1) {
+                        prev->next = l2;
+                        break;
+                    } else if (!l2) {
+                        prev->next = l1;
+                        break;
+                    } else if (l1->val <= l2->val) {
+                        prev->next = l1;
+                        l1 = l1->next;
+                    } else {
+                        prev->next = l2;
+                        l2 = l2->next;
+                    }
+                    prev = prev->next;
+                }
+
+                while (prev->next) prev = prev->next;
+                prev->next = tail;
             }
-            if (!l2) {
-                prev->next = l1;
-                break;
-            }
-            if (l1->val <= l2->val) {
-                prev->next = l1;
-                l1 = l1->next;
-            } else {
-                prev->next = l2;
-                l2 = l2->next;
-            }
-            prev = prev->next;
+
+            if (cnt <= 1) break;
         }
 
         return dummy->next;
