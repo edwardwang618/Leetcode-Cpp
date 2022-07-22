fe62916ad551bbff96f8da7c34849e650466d183
edwardwang618 <edward.wang618@gmail.com>
2022-07-22 00:17:09 -0700

    start from scratch

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -6,16 +6,9 @@
 
 // @lc code=start
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
-        for (int i = 0; i < nums.size(); i++) {
-            if (mp.count(target - nums[i])) return {mp[target - nums[i]], i};
-            mp[nums[i]] = i;
-        }
-
-        return {};
-    }
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    
+  }
 };
 // @lc code=end
-
